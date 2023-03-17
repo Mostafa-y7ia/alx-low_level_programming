39ac9b545dd137edcb316a3b40894df8ff4ae85b
Mostafa-y7ia <mostafayahia199@gmail.com>
2023-03-16 18:48:21 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main () {
-	
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function
+ *
+ * Return: Alawys 0 (success)
+*/
+
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
-
-
 }
