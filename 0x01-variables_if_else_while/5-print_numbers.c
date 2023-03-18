37cc56c9b86335647ea99bbb633ceeae18104a74
Mostafa-y7ia <mostafayahia199@gmail.com>
2023-03-17 19:25:42 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints numbers from 0 to 10
+ *
+ * Return always 0
+ *
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n == 11)
+	{
+		printf("%d", n);
+		n++;
+	}
+		printf("\n");
+	return (0);
+}
