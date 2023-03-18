a791cdffbb2629ac05e46071ae7e7f3e5acaf9bf
Mostafa-y7ia <mostafayahia199@gmail.com>
2023-03-17 19:34:06 -0700

    commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints numbers from 0 to 10
+ *
+ * Return: Alawys 0 (success)
+ *
+*/
+
+int main(void)
+{
+	int n = 48;
+
+	do {
+		putchar(n);
+		n++;
+	} while (n < 58);
+		putchar('\n');
+	return (0);
+}
