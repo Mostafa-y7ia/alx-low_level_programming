efb9b0886fa1aeb862177e86b0cdbeee56e27317
Mostafa-y7ia <mostafayahia199@gmail.com>
2023-03-17 17:16:26 -0700

    commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that assign a random number to the variable
+ *
+ * Return: Alawys 0 (success)
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		puts("is positive\n");
+	if (n == 0)
+		puts("is zero\n");
+	if (n < 0)
+		puts("is negative\n");
+	return (0);
+}
