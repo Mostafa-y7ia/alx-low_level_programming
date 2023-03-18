7c0fee66dd10538a3faa638a3989c8700d2206c4
Mostafa-y7ia <mostafayahia199@gmail.com>
2023-03-17 19:05:28 -0700

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawys 0 (success)
+*/
+
+int main(void)
+{
+	int n = 97;
+
+	do {
+		putchar(n);
+		n++;
+	} while (n < 123);
+	n = 65;
+		do {
+			putchar(n);
+			n++;
+		} while (n < 91);
+		putchar('\n');
+	return (0);
+}
