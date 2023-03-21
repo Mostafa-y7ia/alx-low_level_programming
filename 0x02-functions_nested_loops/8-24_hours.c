50090cb4c9a138c8e5ca029e860f9db06aca62a9
Mostafa-y7ia <mostafayahia199@gmail.com>
2023-03-21 13:36:17 -0700

    commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,24 +6,18 @@
 */
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int i, j;
 
-	for (a = 0; a < 3; a++)
+	for (i = 0; i < 24; i++)
 	{
-		for (b = 0; b < 4; b++)
+		for (j = 0; j < 60; j++)
 		{
-			for (c = 0; c < 6; c++)
-			{
-				for (d = 0; d < 10; d++)
-				{
-					_putchar (a + 48);
-					_putchar (b + 48);
-					_putchar (':');
-					_putchar (c + 48);
-					_putchar (d + 48);
-					_putchar ('\n');
-				}
-			}
+			_putchar(i / 10 + 48);
+			_putchar(i % 10 + 48);
+			_putchar(':');
+			_putchar(j / 10 + 48);
+			_putchar(j % 10 + 48);
+			_putchar('\n');
 		}
 	}
 }
