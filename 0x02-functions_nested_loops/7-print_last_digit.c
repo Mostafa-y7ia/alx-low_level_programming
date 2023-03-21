63b78af9676998ba784958acd658434e4243fd5a
Mostafa-y7ia <mostafayahia199@gmail.com>
2023-03-21 12:54:10 -0700

    commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,28 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * print_last_digit - returns the last digit
+ * _abs - return abslute value
  * @n: integer to be tested
  * Return: integer
 */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (-1 * n);
+	else
+		return (n);
+}
+/**
+ * print_last_digit - pritns the last digit of a number
+ * @n: integer whose last digit is to be printed
+ * Return: lastdigit of n
+ */
 int print_last_digit(int n)
 {
-	int x;
+	int lastDigit;
 
-	x = n % 10;
-	printf("%d", x);
-	return (x);
+	lastDigit = _abs((n % 10));
+	_putchar(lastDigit + 48);
+	return (lastDigit);
 }
