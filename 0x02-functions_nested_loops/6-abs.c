c9b259eb9a8d55ba29c8ff3290fc567a3739f442
Mostafa-y7ia <mostafayahia199@gmail.com>
2023-03-21 12:38:16 -0700

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - return abslute value
+ * @n: integer to be tested
+ * Return: integer
+*/
+
+int _abs(int n)
+{
+	int n;
+
+	if (n < 0)
+		return (-1 * n);
+	else
+		return (n);
+}
