ee08c9ff462316403d56c10d2b879cb82210d1ba
Mostafa-y7ia <mostafayahia199@gmail.com>
2023-03-30 01:44:13 -0700

    s

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of string
+ * @str: char prameter as pointer
+*/
+
+void puts_half(char *str)
+{
+	int i = 0, n;
+
+	while (str[i] != '\0')
+	(
+		 i++;
+	)
+
+	if (n % 2 == 0)
+	{
+		n = i / 2;
+	}
+	else
+		n = (i - 1) / 2;
+
+	for (j = n; j < i; j++)
+		_putchar(str[j]);
+	_putchar('\n');
+}
