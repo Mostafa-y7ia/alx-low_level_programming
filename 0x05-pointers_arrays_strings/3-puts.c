9caecf57425fb7ee35a779a3098d05eee986679a
Mostafa-y7ia <mostafayahia199@gmail.com>
2023-03-29 21:59:37 -0700

    s

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,19 @@
 #include "main.h"
 
-/***/
+/**
+ * _puts - prints string
+ * @str: char parameter as pointer
+*/
 
 void _puts(char *str)
 {
 	int n, i = 0;
 
-	i = _strlen(str);
-	for(n = 0; n<i; n++)
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (n = 0; n < i; n++)
 		_putchar(str[n]);
 	_putchar('\n');
 }
