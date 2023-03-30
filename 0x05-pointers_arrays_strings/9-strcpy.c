03254ea85d9c93e25dc29433a8be975afbb8a990
Mostafa-y7ia <mostafayahia199@gmail.com>
2023-03-30 13:02:39 -0700

    commit3

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the array
+ * @dest: char prameter as pointer
+ * @src: char prameter as pointer
+ * Return: char array
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		src[i] = dest[i];
+	} while (src[i] != '\0')
+
+	return (dest);
+}
