4c01d3b6faacfdd1323d3495546936f7dd07d76e
Mostafa-y7ia <mostafayahia199@gmail.com>
2023-03-30 12:34:12 -0700

    commit3

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * print_array - prints an array elemnts
+ * @a: int prameter as pointer
+ * @n: int prameter
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		printf("%d, ", a[i]);
+	putchar('\n');
+}
