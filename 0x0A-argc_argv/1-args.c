d5ad4d2298b639f0dc3ae747f1c2efad708a3ab7
Mostafa-y7ia <mostafayahia199@gmail.com>
2023-04-10 21:45:27 -0700

    commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: integer prameter
+ * @argv: char array
+ * Return: integer
+*/
+int main(int argc, char const *argv[])
+{
+	printf("%d\n", argc - 1);
+	(void)argv;
+	return (0);
+}
